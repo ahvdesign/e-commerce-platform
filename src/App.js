390bca09f989522c8e39237d65d19b1fa95a3f83
Alexander Henriquez <ahvdesign@gmail.com>
2023-05-10 18:45:08 -0400

    created directory component and category items

--- src/App.js
+++ src/App.js
@@ -1,53 +1,37 @@
+import "./categories.styles.scss";
+import Directory from "./components/directory/directory.component";
+
 const App = () => {
-  return (
-    <div>
-    <div className='categories-container'>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>D&D Minis</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-    </div>
-    <div className='categories-container'>
-    <div className="category-container">
-      {/* <img /> */}
-      <div className="category-body-container">
-        <h2>Warhammer Minis</h2>
-        <p>Shop Now</p>
-      </div>
-    </div>
-  </div>
-  <div className='categories-container'>
-  <div className="category-container">
-    {/* <img /> */}
-    <div className="category-body-container">
-      <h2>Environment Objects</h2>
-      <p>Shop Now</p>
-    </div>
-  </div>
-</div>
-<div className='categories-container'>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Tiles</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-    </div>
-    <div className='categories-container'>
-    <div className="category-container">
-      {/* <img /> */}
-      <div className="category-body-container">
-        <h2>Miscellaneous</h2>
-        <p>Shop Now</p>
-      </div>
-    </div>
-  </div>
-  </div>
-  );
-}
+  const categories = [
+    {
+      id: 1,
+      title: "D&D Minis",
+      imageUrl: "https://i.imgur.com/XqI3WFV.jpeg",
+    },
+    {
+      id: 2,
+      title: "Warhammer Minis",
+      imageUrl: "https://i.imgur.com/BTeQiHU.jpeg",
+    },
+    {
+      id: 3,
+      title: "Environment Props",
+      imageUrl:
+        "https://i.pinimg.com/736x/7d/62/e7/7d62e7f03e2ac7b05da105fb9c3ff3f1.jpg",
+    },
+    {
+      id: 4,
+      title: "Miscellaneous",
+      imageUrl: "https://i.imgur.com/HlrZUUo.jpeg",
+    },
+    {
+      id: 5,
+      title: "Custom Order",
+      imageUrl: "https://i.imgur.com/uzilltj.jpeg",
+    },
+  ];
+
+  return <Directory categories={categories} />;
+};
 
 export default App;
